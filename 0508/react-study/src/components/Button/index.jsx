02d05fdbc8461334959801ec3b90e4a5ee4f9c16
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-08 16:03:09 +0900

    0508

---
+++ 0508/react-study/src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import * as S from './styled';
+
+function Button(props){
+   return(
+    <>
+        <S.Button>{props.children}</S.Button>
+        {props.color} / {props.size}
+    </>
+
+   );
+}
+
+export default Button;
