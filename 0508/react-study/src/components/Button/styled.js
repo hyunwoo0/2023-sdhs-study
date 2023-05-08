626e551ff5fcecb5610c3bd23aa3331cf9cfca79
hyunwoo0 <121408661+hyunwoo0@users.noreply.github.com>
2023-05-08 16:08:12 +0900

    Revert "0508"

--- 0508/react-study/src/components/Button/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-`;
--- 0508/react-study/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom/client";
-import Router from "./pages/router";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
