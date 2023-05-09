23f5190803f3ebe5e9707a046053c2cf73ddcd09
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-09 09:25:18 +0900

    0509

---
+++ 0508/react-study/src/pages/main/index.jsx
@@ -0,0 +1,18 @@
+import Button from '../../components/Button';
+
+import * as S from './styled';
+
+
+
+function Main() {
+    return(
+    <S.Container>
+        <S.MainWrapper>
+            <div>메인페이지</div>
+            <Button>만들기</Button>
+        </S.MainWrapper>
+    </S.Container>
+    );
+}
+
+export default Main;
