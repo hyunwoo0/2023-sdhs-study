707bcde7da53b2b8c548e88d0003fe5c09eda12a
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-12 14:34:30 +0900

    [DOCS]주석 추가

--- 0508/react-study/src/components/SearchInput/index.jsx
+++ 0508/react-study/src/components/SearchInput/index.jsx
@@ -6,7 +6,10 @@ function SearchInput({ onChange }) {
   const [value, setValue] = useState("");
 
   const handleValue = ({ target: { value } }) => {
+    //1. input에서 입력한 값
     setValue(value);
+
+    //2. onChange라는 함수에 input에서 입력한 값을 넘겨줌
     onChange?.(value);
   };
 
