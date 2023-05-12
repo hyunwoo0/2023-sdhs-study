c8c8b38c2d8192a980bc242c6ef45185e32b2b03
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-12 10:31:24 +0900

    [FEAT] 검색 기능

---
+++ 0508/react-study/src/components/SearchInput/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import * as S from "./styled";
+
+function SearchInput({ onChange }) {
+  const [value, setValue] = useState("");
+
+  const handleValue = ({ target: { value } }) => {
+    setValue(value);
+    onChange?.(value);
+  };
+
+  return (
+    <S.SearchInputBox>
+      <S.SearchInput
+        placeholder="검색할 내용을 입력하세요."
+        value={value}
+        onChange={handleValue}
+      />
+    </S.SearchInputBox>
+  );
+}
+
+export default SearchInput;
