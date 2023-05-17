6ad4d0ae1e4539e8414f8b59fd979a6d934c73e4
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-17 11:02:52 +0900

    [STYLE] main

---
+++ 0508/react/src/pages/main/styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import { Button } from '../../../../react/src/components/Button/styled';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  padding: 16px;
+  box-shadow: 0 0 4.5px rgba(156, 156, 156, 0.4);
+  > div {
+    font-size: 24px;
+    font-weight: 700;
+    &:hover {
+      color: red;
+    }
+  }
+  > ${Button} {
+    padding: 8px 10px;
+    boreder: none;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover {
+      color: #d6d6d6;
+    }
+    > a {
+      color: #000000;
+      text-decoration: none;
+    }
+  }
+`;
