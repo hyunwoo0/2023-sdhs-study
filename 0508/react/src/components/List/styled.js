6f0ae642be847fc017890170a9e6ed5ede9b9ac5
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-17 10:59:12 +0900

    [STYLE] component List

---
+++ 0508/react/src/components/List/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 15px;
+
+  margin-top: 10px;
+  border: 1px solid black;
+  border-radius: 5px 5px;
+  list-style: none;
+`;
+
+export const Title = styled.p`
+  font-size: 16px;
+  padding-left: 5px;
+`;
