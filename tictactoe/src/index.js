1aea48e23cdfac3149bf32d43bce8f0722f185c5
hyunwoo0 <baghyeonu123@gmail.com>
2023-05-18 15:12:36 +0900

    [DOCS] main index

---
+++ tictactoe/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TicTacToe from "./TicTacToe";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<TicTacToe />);
